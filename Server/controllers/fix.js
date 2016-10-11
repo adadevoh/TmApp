b9217b823888ef235feb696261df809eb2d47a78
Joshua Adadevoh <tsc.joshua@gmail.com>
2016-10-11 16:08:10 -0400

    working on fix.save

--- Server/controllers/fix.js
+++ Server/controllers/fix.js
@@ -1,27 +1,29 @@
-﻿var model = require( '../models/base' );
+﻿var base = require( '../models/base' );
 var fix = require('../models/fix');
 
 exports.add = function (req, res) {
 };
 
 exports.save = function ( req, res ) {
+    var model = new base();
+    model.tableName = 'fixes';
     console.log(req.body);
     console.log('fix save called');
     console.log();
 
     var j  = 1;
     var objs = [];
-    var obj = {};
+    var singleFixObject = {};
     for (var key in req.body){
         console.log(key +" : "+req.body[key])
         console.log('key slice: ' + key.slice(7))
-        obj[key.slice(7)] = req.body[key]
+        singleFixObject[key.slice(7)] = req.body[key]
         if(j%5 == 0){
             console.log();
-            obj['fixNumber'] = key.slice(0,6);
-            objs.push(obj);
+            singleFixObject['fixNumber'] = key.slice(0,6);
+            objs.push(singleFixObject);
             j = 1;
-            obj = {};
+            singleFixObject = {};
             continue;
         }
             
@@ -30,6 +32,26 @@ exports.save = function ( req, res ) {
 
     console.log('objs: ')
     console.log(objs)
+    var i = 0;
+    for(var obj in objs){
+        console.log();
+        console.log('obj: ');
+        console.log(objs[i]);
+        console.log();
+        console.log('i: '+ i);
+        console.log(objs[i].LIV);
+        i++
+        /*model.update([ { liv:obj['Liv'], regression:obj['Regression'], stress:obj['Stress'], whql:obj['Whql'], readme:obj['ReadMe'] }, {fixNumber:obj['fixNumber']} ], function(err, results){
+            if(err){
+                console.log('fi update error!');
+                console.log(err);
+            }
+        })*/
+    }
 
     res.redirect('/');
 };
+
+function parseFixSaveFormData(){
+
+}
