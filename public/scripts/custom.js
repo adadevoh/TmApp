89154722e1d1278e0a75dd1780e15518147de85b
Joshua Adadevoh <tsc.joshua@gmail.com>
2016-11-08 16:14:00 -0500

    added login validation
    needs some more testing to make sure i'm not interfering with any other
    forms

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -15,7 +15,43 @@ $.ajax({
     }
 })
 
-
+//login form validation
+$(document)
+    .ready(function() {
+      $('.ui.form')
+        .form({
+          fields: {
+            username: {
+              identifier  : 'username',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your userid'
+                }/*,
+                {
+                  type   : 'username',
+                  prompt : 'Please enter a valid e-mail'
+                }*/
+              ]
+            },
+            password: {
+              identifier  : 'password',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your password'
+                },
+                {
+                  type   : 'length[6]',
+                  prompt : 'Your password must be at least 6 characters'
+                }
+              ]
+            }
+          }
+        })
+      ;
+    })
+  ;
 
 function test_Complete(fixID){
     console.log("test complete: "+fixID);
