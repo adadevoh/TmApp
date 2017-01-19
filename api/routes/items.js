106df5a3b0b1215090a0eb9561b74ea4b8f0e470
Joshua Adadevoh <tsc.joshua@gmail.com>
2017-01-19 15:13:17 -0500

    removed route: api/items as it is currently redundant
    the current app logic is to have an api simply for reading data from the
    db. the route: api/index will handle that. For now this design is
    satisfactory

--- api/routes/items.js
+++
@@ -1,7 +0,0 @@
-var express = require( 'express' );
-var router = express.Router();
-var itemCtrl = require('../controllers/items.js');
-
-router.get('/itemList/:user', itemCtrl.itemsList);
-
-module.exports = router;
